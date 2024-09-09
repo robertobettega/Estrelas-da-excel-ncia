e278c0e8ce8d99b4e1365cf65ff50086216968fe
Roberto Bettega Medeiros Peixoto <robertobettega@NATALCENTER.LOCAL>
2024-09-09 17:16:17 -0300

    Tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,156 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Estrelas da excelência</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
 </head>
+
 <body>
-    
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div>
+                <img src="http://10.1.2.110:80/centralservicos/resources/img/central-servicos.png"
+                    alt="Hospital Rio Grande" style="width: 160px">
+            </div>
+            <div>
+                <img src="imagens/Logo estrelas da excelencia.png" alt="Estrelas da Excelência" style="width: 160px">
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Roberto Bettega
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Meus dados</a></li>
+                    <li><a class="dropdown-item" href="#">Ouvidoria</a></li>
+                    <li><a class="dropdown-item" href="#">Ajuda</a></li>
+                    <li><a class="dropdown-item" href="#">Sair</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid text-center justify-content-center" style="display: flex; margin: 15px">
+        <div>
+            <img src="imagens/Hospitalidade.png" style="width: 160px; margin: 15px" alt="Hospitalidade">
+            <div>Hospitalidade</div>
+        </div>
+        <div>
+            <img src="imagens/Prestreza.png" style="width: 160px; margin: 15px" alt="Prestreza">
+            <div>Prestreza</div>
+        </div>
+        <div>
+            <img src="imagens/Inovação.png" style="width: 160px; margin: 15px" alt="Inovação">
+            <div>Inovação</div>
+        </div>
+        <div>
+            <img src="imagens/Segurança.png" style="width: 160px; margin: 15px" alt="Segurança">
+            <div>Segurança</div>
+        </div>
+    </div>
+
+    <div class="card" style="margin: 15px; padding: 15px">
+
+        <div class="container-fluid row justify-content-center">
+            <div class="card mb-3" style="max-width: 450px; padding: 15px; margin: 15px">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="imagens/Hospitalidade.png" style="width: 140px" alt="Hospitalidade">
+                        <div>Hospitalidade</div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Roberto Bettega <span
+                                        class="badge rounded-pill text-bg-success">1º</span></li>
+                                <li class="list-group-item">Gabriel Mendes <span
+                                        class="badge rounded-pill text-bg-success">2º</span></li>
+                                <li class="list-group-item">Pedro Nelson <span
+                                        class="badge rounded-pill text-bg-success">3º</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="card mb-3" style="max-width: 450px; padding: 15px; margin: 15px">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="imagens/Prestreza.png" style="width: 140px" alt="Prestreza">
+                        <div>Prestreza</div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Roberto Bettega <span
+                                        class="badge rounded-pill text-bg-success">1º</span></li>
+                                <li class="list-group-item">Gabriel Mendes <span
+                                        class="badge rounded-pill text-bg-success">2º</span></li>
+                                <li class="list-group-item">Pedro Nelson <span
+                                        class="badge rounded-pill text-bg-success">3º</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 450px; padding: 15px; margin: 15px">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="imagens/Inovação.png" style="width: 140px" alt="Inovação">
+                        <div>Inovação</div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Roberto Bettega <span
+                                        class="badge rounded-pill text-bg-success">1º</span></li>
+                                <li class="list-group-item">Gabriel Mendes <span
+                                        class="badge rounded-pill text-bg-success">2º</span></li>
+                                <li class="list-group-item">Pedro Nelson <span
+                                        class="badge rounded-pill text-bg-success">3º</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 450px; padding: 15px; margin: 15px">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="imagens/Segurança.png" style="width: 140px" alt="Segurança">
+                        <div>Segurança</div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Roberto Bettega <span
+                                        class="badge rounded-pill text-bg-success">1º</span></li>
+                                <li class="list-group-item">Gabriel Mendes <span
+                                        class="badge rounded-pill text-bg-success">2º</span></li>
+                                <li class="list-group-item">Pedro Nelson <span
+                                        class="badge rounded-pill text-bg-success">3º</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 // Route::get('/login', [HomeController::class, 'index'])->name('user.index');
 Route::get('/home', [HomeController::class, 'HomePage']);
-
