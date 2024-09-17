544e16fdfd2f58e20a0a41d8b2a277c02ff92f2f
vitpinheiro <vitoriapinheiro775@gmail.com>
2024-09-17 13:29:22 -0300

    ajuste painel

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -14,24 +14,30 @@
         @method('put')
 
         <div>
-            <x-input-label class="form-label" for="update_password_current_password" :value="__('Current Password')" />
+        <div class="col-md-8">
+            <x-input-label class="form-label mt-2" for="update_password_current_password" :value="__('Current Password')" />
             <x-text-input class="form-control col-3" id="update_password_current_password" name="current_password" type="password" autocomplete="current-password" />
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
+        </div>
 
         <div>
-            <x-input-label class="form-label" for="update_password_password" :value="__('New Password')" />
+        <div class="col-md-8">
+            <x-input-label class="form-label mt-2" for="update_password_password" :value="__('New Password')" />
             <x-text-input class="form-control" id="update_password_password" name="password" type="password" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
+        </div>
 
         <div>
-            <x-input-label class="form-label" for="update_password_password_confirmation" :value="__('Confirm Password')" />
+        <div class="col-md-8">
+            <x-input-label class="form-label mt-2" for="update_password_password_confirmation" :value="__('Confirm Password')" />
             <x-text-input class="form-control" id="update_password_password_confirmation" name="password_confirmation" type="password" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
+        </div>
 
-        <div class="flex items-center gap-4">
+        <div class="flex items-center gap-4 mt-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,64 +1,66 @@
+<header>
+    <h2>
+        {{ __('Profile Information') }}
+    </h2>
 
+    <p>
+        {{ __("Update your account's profile information and email address.") }}
+    </p>
+</header>
 
-    <header>
-        <h2>
-            {{ __('Profile Information') }}
-        </h2>
+<form id="send-verification" method="post" action="{{ route('verification.send') }}">
+    @csrf
+</form>
 
-        <p>
-            {{ __("Update your account's profile information and email address.") }}
-        </p>
-    </header>
+<form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-4">
+    @csrf
+    @method('patch')
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
-
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
-
-        <div>
-            <x-input-label for="name" :value="__('Name')" class="form-label" />
+    <div class="mb-3">
+    <div class="col-md-8"> 
+        <x-input-label for="name" :value="__('Name')" class="form-label" />
             <x-text-input id="name" name="name" type="text" class="form-control" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
-        </div>
+        <x-input-error class="mt-2" :messages="$errors->get('name')" />
+    </div>
+    </div>
 
-        <div>
-            <x-input-label for="email" :value="__('Email')" class="form-label"/>
+    <div class="mb-3">
+    <div class="col-md-8"> 
+        <x-input-label for="email" :value="__('Email')" class="form-label"/>
             <x-text-input id="email" name="email" type="email" class="form-control" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
+        <x-input-error class="mt-2" :messages="$errors->get('email')" />
+   
+        @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            <div class="mt-3">
+                <p class="text-sm text-gray-800">
+                    {{ __('Your email address is unverified.') }}
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+                    <button form="send-verification" class="btn btn-primary btn-sm">
+                        {{ __('Click here to re-send the verification email.') }}
+                    </button>
+                </p>
 
-                        <button form="send-verification" class="entrar">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
+                @if (session('status') === 'verification-link-sent')
+                    <p class="mt-2 text-sm text-success">
+                        {{ __('A new verification link has been sent to your email address.') }}
                     </p>
+                @endif
+            </div>
+        @endif
+    </div>
+    </div>
 
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
-        </div>
+    <div class="d-flex align-items-center gap-3 mt-4">
+        <x-primary-button class="btn-sm">{{ __('Save') }}</x-primary-button>
 
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
-            @endif
-        </div>
-    </form>
+        @if (session('status') === 'profile-updated')
+            <p
+                x-data="{ show: true }"
+                x-show="show"
+                x-transition
+                x-init="setTimeout(() => show = false, 2000)"
+                class="text-sm text-gray-600"
+            >{{ __('Saved.') }}</p>
+        @endif
+    </div>
+</form>
