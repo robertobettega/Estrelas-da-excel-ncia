3fde3191087b4e2017e1cec1a6ca6a1740a35519
vitpinheiro <vitoriapinheiro775@gmail.com>
2024-09-17 13:39:19 -0300

    Revert "Merge branch 'master' of https://github.com/dti-hrg/estrelasexcelencia into vit"
    This reverts commit 3b24f366deaa5202ff08581a7fe10289d1c6dc7a, reversing
    changes made to 544e16fdfd2f58e20a0a41d8b2a277c02ff92f2f.

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -7,7 +7,6 @@
         <p class="mt-1 text-sm text-gray-600">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
-        <img src="images/Divisória Degradê (9).png" alt="">
     </header>
 
     <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
@@ -15,43 +14,40 @@
         @method('put')
 
         <div>
-            <x-input-label class="form-label" for="update_password_current_password" :value="__('Current Password')" />
-            <div class="col-md-6">
-                <x-text-input class="form-control col-3" id="update_password_current_password" name="current_password"
-                    type="password" autocomplete="current-password" />
-            </div>
-            <div class="col-md-6">
-                <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
-            </div>
+        <div class="col-md-8">
+            <x-input-label class="form-label mt-2" for="update_password_current_password" :value="__('Current Password')" />
+            <x-text-input class="form-control col-3" id="update_password_current_password" name="current_password" type="password" autocomplete="current-password" />
+            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
         </div>
 
         <div>
-            <x-input-label class="form-label" for="update_password_password" :value="__('New Password')" />
-            <div class="col-md-6">
-                <x-text-input class="form-control" id="update_password_password" name="password" type="password"
-                    autocomplete="new-password" />
-            </div>
+        <div class="col-md-8">
+            <x-input-label class="form-label mt-2" for="update_password_password" :value="__('New Password')" />
+            <x-text-input class="form-control" id="update_password_password" name="password" type="password" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
         </div>
 
         <div>
-            <x-input-label class="form-label" for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <div class="col-md-6">
-                <x-text-input class="form-control" id="update_password_password_confirmation"
-                    name="password_confirmation" type="password" autocomplete="new-password" />
-            </div>
+        <div class="col-md-8">
+            <x-input-label class="form-label mt-2" for="update_password_password_confirmation" :value="__('Confirm Password')" />
+            <x-text-input class="form-control" id="update_password_password_confirmation" name="password_confirmation" type="password" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
         </div>
 
-        <div class="flex items-center gap-4">
-            <button class="entrar"><b>{{ __('Save') }}</b></button>
+        <div class="flex items-center gap-4 mt-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
-                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600">{{ __('Saved.') }}</p>
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600"
+                >{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
