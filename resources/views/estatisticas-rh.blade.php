bb5ace744bc6d652728c30bc357b671abac31413
imgabrielmendes <imgabrielmendes1@gmail.com>
2024-09-20 07:51:19 -0300

    Estatísticas-rh usando os usuários do Banco de dados

--- resources/views/estatisticas-rh.blade.php
+++ resources/views/estatisticas-rh.blade.php
@@ -11,47 +11,6 @@
 
 <div class="card" style="margin: 30px">
     <div class="card" style="margin: 30px">
-        <div class="d-flex justify-content-evenly">
-            <div class="col-4 justify-content-center">
-                <label>Excelência:</label>
-                <div>
-                    {{-- CAIXA DE OPÇÕES QUE PEGA AS EXCELÊNCIAS --}}
-
-                    <select class="col form-select col-6" aria-label="Selecione o Usuário" id="caixaexcelencia">
-                        <option selected disabled>Escolha a excelência</option>
-                        @foreach ($excelencias_opcoes as $opcoes)
-                            <option value="{{ $opcoes->id }}">
-                                {{ $opcoes->DESCRICAO }}
-                                {{-- ID:({{ $opcoes->id }}) --}}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-
-            <div class="col-4 justify-content-center">
-                <label>Mês:</label>
-                <div>
-                    {{-- CAIXA DE OPÇÕES QUE PEGA AS EXCELÊNCIAS --}}
-
-                    <select class="col form-select" aria-label="Selecione o Usuário" id="caixaexcelencia">
-                        <option selected disabled>Escolha o mês</option>
-                        <option value="1">Janeiro</option>
-                        <option value="2">Fevereiro</option>
-                        <option value="3">Março</option>
-                        <option value="4">Abril</option>
-                        <option value="5">Maio</option>
-                        <option value="6">Junho</option>
-                        <option value="7">Julho</option>
-                        <option value="8">Agosto</option>
-                        <option value="9">Setembro</option>
-                        <option value="10">Outubro</option>
-                        <option value="11">Novembro</option>
-                        <option value="12">Dezembro</option>
-                    </select>
-                </div>
-            </div>
-        </div>
     <div class="table-responsive">
     <table class="table table-striped table-hover">
         <thead>
@@ -65,26 +24,14 @@
           </thead>
           <tbody>
             <tr>
-                <th scope="row">1</th>
-                <td>Roberto Bettega Medeiros Peixoto</td>
-                <td>robertopeixoto@hospitalriogrande.com.br</td>
+                @foreach ($users as $item)
+                <th scope="row">{{ $item->id }}</th>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->email }}</td>
                 <td>6747</td>
                 <td>8</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Gabriel Mendes sei lá de quê</td>
-                <td>gabrielsousa@hospitalriogrande.com.br</td>
-                <td>num sei</td>
-                <td>0</td>
-            </tr>
-            <tr>
-                <th scope="row">1</th>
-                <td>Vitória Vic</td>
-                <td>acho que vitoria n tem email institucional</td>
-                <td>nem matricula</td>
-                <td>1</td>
-            </tr>
+                </tr>
+            @endforeach
       </table>
     </div>
     
