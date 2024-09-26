25ebb624f36a58d84b453a33a8946d27c7a69469
robertobettega <bettega_medeiros@hotmail.com>
2024-09-26 10:58:27 -0300

    Mudar cursos de imagem da exceleência para pointer para indicar que [e clicável

--- resources/views/assets/cards-excelencia.blade.php
+++ resources/views/assets/cards-excelencia.blade.php
@@ -1,7 +1,7 @@
 @foreach ($excelencias_opcoes as $excelencia)
     <div>
         <!-- Adiciona um atributo data-id para armazenar o ID -->
-        <img src="{{ asset($excelencia->ICONES) }}" style="width: 160px; margin: 15px;" alt="{{ $excelencia->DESCRICAO }}"
+        <img src="{{ asset($excelencia->ICONES) }}" style="width: 160px; margin: 15px; cursor: pointer" alt="{{ $excelencia->DESCRICAO }}"
             data-bs-toggle="modal" data-bs-target="#AvaliacaoModal" data-id="{{ $excelencia->id }}" class="excelencia-img">
         <div style="font-weight: 700;">{{ $excelencia->DESCRICAO }}</div>
     </div>
